17ed9eb7c54fb0becbbde1e621f2b18d07cc15c9
Lulle <parmeshwar.lulle@capgemini.com>
2018-01-18 12:27:54 +0530

    ActuatorProject Added

---
+++ SpringBootActuatorDemo/src/main/java/com/capgemini/actuator/demo/SpringBootActuatorDemoApplication.java
@@ -0,0 +1,12 @@
+package com.capgemini.actuator.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootActuatorDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootActuatorDemoApplication.class, args);
+	}
+}
